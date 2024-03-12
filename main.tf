aa401e9819478c90cc6c14e07d58bf09f0efdd29
Elena Huma <85832851+IoanaElenaH@users.noreply.github.com>
2024-03-12 20:51:48 +0200

    add files

---
+++ main.tf
@@ -0,0 +1,18 @@
+data "aws_vpc" "ingress" {
+    default = true
+}
+
+resource "aws_security_group" "ingress" {
+    name        = "test"
+    description = "Allow inbound"
+    vpc_id      = data.aws_vpc.ingress.id
+}
+
+resource "aws_security_group_rule" "ingress" {
+    type        = "ingress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = var.public_cidr_block
+    security_group_id = "${aws_security_group.ingress.id}"
+}
