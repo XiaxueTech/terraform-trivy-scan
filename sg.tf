8327b93d5907d5ebe23e50b8fb91514f53ce06fb
Elena Huma <85832851+IoanaElenaH@users.noreply.github.com>
2024-03-12 20:59:10 +0200

    update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,7 +1,11 @@
+data "aws_vpc" "egress" {
+    default = true
+}
+
 resource "aws_security_group" "egress" {
     name        = "test"
     description = "Allow outbound"
-    vpc_id      = data.aws_vpc.ingress.id
+    vpc_id      = data.aws_vpc.egress.id
 }
 
 resource "aws_security_group_rule" "egress" {
@@ -9,6 +13,6 @@ resource "aws_security_group_rule" "egress" {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
-    cidr_blocks = var.public_cidr_block
+    cidr_blocks = [0.0.0.0/0]
     security_group_id = "${aws_security_group.egress.id}"
 }
