bc61178667ef6e26f4deb93bb10136ee1e53fa13
Elena Huma <85832851+IoanaElenaH@users.noreply.github.com>
2024-03-13 20:00:56 +0200

    test

--- main.tf
+++ main.tf
@@ -16,3 +16,27 @@ resource "aws_security_group_rule" "ingress" {
     cidr_blocks = var.public_cidr_block
     security_group_id = "${aws_security_group.ingress.id}"
 }
+
+resource "aws_security_group" "egress" {
+    name        = "test"
+    description = "Allow outbound"
+    vpc_id      = data.aws_vpc.egress.id
+}
+
+resource "aws_security_group_rule" "egress" {
+    type        = "ingress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = var.public_cidr_block
+    security_group_id = "${aws_security_group.egress.id}"
+}
+
+resource "aws_security_group_rule" "egress" {
+    type        = "ingress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = var.private_cidr_block
+    security_group_id = "${aws_security_group.egress.id}"
+}
